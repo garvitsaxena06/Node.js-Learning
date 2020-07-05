aa998c5f2f9a079753cbb80a7fb82c00c2c3aa8a
Garvit Saxena <garvitsaxena06@gmail.com>
2020-07-05 11:23:11 +0530

    event emit and subscribe

---
+++ freecodecamp.org/Raising_and_Registering_an_event/App.js
@@ -0,0 +1,23 @@
+
+const EventEmitter = require('events')
+
+class Person extends EventEmitter {
+    constructor(Name) {
+        super()
+        this.name = Name
+    }
+}
+
+const mike = new Person('Mike')
+const garvit = new Person('Garvit')
+
+mike.on('callMyName', () => {
+    console.log("My name is", mike.name)
+})
+
+garvit.on('callMyName', () => {
+    console.log("My name is", garvit.name)
+})
+
+mike.emit('callMyName')
+garvit.emit('callMyName')
