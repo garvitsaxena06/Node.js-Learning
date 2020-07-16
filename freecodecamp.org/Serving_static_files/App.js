247cb0ef7a410b92e2064d8c1f7f1ad6868efb97
Garvit Saxena <garvitsaxena06@gmail.com>
2020-07-16 23:52:46 +0530

    reading json data using http module

--- freecodecamp.org/Serving_static_files/App.js
+++ freecodecamp.org/Serving_static_files/App.js
@@ -2,8 +2,8 @@ const http = require('http')
 const fs = require('fs')
 
 http.createServer((req, res) => {
-    const readStream = fs.createReadStream('./static/index.html')
-    res.writeHead(200, {'Content-type': 'text/html'})
+    const readStream = fs.createReadStream('./static/trial.json')
+    res.writeHead(200, {'Content-type': 'application/json'})
     readStream.pipe(res)
 }).listen(3000)
 console.log('Listening to post 3000')
