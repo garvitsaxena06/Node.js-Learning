0d6da6c81fc3fd1e807978a2b645e3fa97c8dc9b
Garvit Saxena <garvitsaxena06@gmail.com>
2020-07-17 01:57:08 +0530

    route params and query strings

--- freecodecamp.org/Express_server/App.js
+++ freecodecamp.org/Express_server/App.js
@@ -6,5 +6,15 @@ app.get('/', (req, res) => {
     res.send("Hello World")
 })
 
+app.get('/example', (req, res) => {
+    res.send("Hitting the example")
+})
+
+app.get('/example/:name/:age', (req, res) => {
+    console.log(req.params)
+    console.log(req.query)
+    res.send(req.params.name + ":" + req.params.age)
+})
+
 app.listen(3000)
 console.log("Listening to post 3000!")
