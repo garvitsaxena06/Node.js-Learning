8f2a98d07113b0e03605fd2c91f90e03dc0928ad
Garvit Saxena <garvitsaxena06@gmail.com>
2020-07-16 23:53:18 +0530

    reading image using http module

--- freecodecamp.org/Serving_static_files/App.js
+++ freecodecamp.org/Serving_static_files/App.js
@@ -2,8 +2,8 @@ const http = require('http')
 const fs = require('fs')
 
 http.createServer((req, res) => {
-    const readStream = fs.createReadStream('./static/trial.json')
-    res.writeHead(200, {'Content-type': 'application/json'})
+    const readStream = fs.createReadStream('./static/IMG.jpeg')
+    res.writeHead(200, {'Content-type': 'image/jpeg'})
     readStream.pipe(res)
 }).listen(3000)
 console.log('Listening to post 3000')
