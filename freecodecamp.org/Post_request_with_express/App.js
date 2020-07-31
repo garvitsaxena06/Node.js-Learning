3585436acae3e1a15b19f4eead5ff691af1ce814
Garvit Saxena <garvitsaxena06@gmail.com>
2020-07-31 22:04:32 +0530

    POST request from express done

--- freecodecamp.org/Post_request_with_express/App.js
+++ freecodecamp.org/Post_request_with_express/App.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 
 const app = express()
 const PORT = 3000
 
 app.use('/public', express.static(path.join(__dirname, '/static')))
+app.use(bodyParser.urlencoded({extended: false}))
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'static', 'index.html'))
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.send("Successfully send the data!")
 })
 
 app.listen(PORT, err => {
