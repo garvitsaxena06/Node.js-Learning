5b4d51ed13807929b675270ac81bb28185530214
Garvit Saxena <garvitsaxena06@gmail.com>
2020-07-31 21:45:08 +0530

    POST request form created

---
+++ freecodecamp.org/Post_request_with_express/App.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const PORT = 3000
+
+app.use('/public', express.static(path.join(__dirname, '/static')))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'index.html'))
+})
+
+app.listen(PORT, err => {
+    if (err) {
+        console.log(err)
+    }
+    else {
+        console.log(`Listening to port ${ PORT }`)
+    }
+})
