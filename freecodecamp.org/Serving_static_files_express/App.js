dddc5920ad179fe09b8aafc751882bc28d3ae19e
Garvit Saxena <garvitsaxena06@gmail.com>
2020-07-31 20:30:05 +0530

    serving static files completed

--- freecodecamp.org/Serving_static_files_express/App.js
+++ freecodecamp.org/Serving_static_files_express/App.js
@@ -3,10 +3,18 @@ const path = require('path')
 
 const app = express()
 
-app.use('/public', express.static(path.join(__dirname, 'static')))          //__dirname shows the path of App.js
+const PORT = 3000
+
+app.use('/public', express.static(path.join(__dirname, '/static')))        //__dirname shows the path of App.js
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'static', 'index.html'))
 })
 
-app.listen(3000)
-console.log("Listening to port 3000!")
+app.listen(PORT, err => {
+    if (err) {
+        console.log(err)
+    }
+    else {
+        console.log(`Listening to port ${ PORT }`)
+    }
+})
