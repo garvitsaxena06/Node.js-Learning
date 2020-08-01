0561498c0f1513af216739fce885facc74e92544
Garvit Saxena <garvitsaxena06@gmail.com>
2020-08-01 11:39:52 +0530

    JOI input validation

--- freecodecamp.org/Validating_using_JOI/App.js
+++ freecodecamp.org/Validating_using_JOI/App.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path')
+const Joi = require('joi')
 const bodyParser = require('body-parser')
+const { join } = require('path')
 
 const app = express()
 const PORT = 3000
@@ -15,14 +17,28 @@ app.get('/', (req, res) => {
 
 app.post('/post', (req, res) => {
     console.log(req.body)
-    res.json({success : true})
+    const schema = Joi.object().keys({
+        email : Joi.string().email().required(),
+        password : Joi.string().min(5).max(10)
+    })
+    
+    Joi.validate(req.body, schema, (err, result) => {
+        if (err) {
+            console.log(err)
+            res.send("An error occured!")
+        }
+        else {
+            console.log(result)
+            res.send("Successfully posted data!")
+        }
+    })
 })
 
 app.listen(PORT, err => {
     if (err) {
         console.log(err)
     }
-    else {  
+    else {
         console.log(`Listening to port ${ PORT }`)
     }
 })
