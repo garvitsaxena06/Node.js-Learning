e8db6f6fa2bb547edeb188635d6763f18027bf11
Garvit Saxena <garvitsaxena06@gmail.com>
2020-08-03 22:49:59 +0530

    EJS_template setup

--- freecodecamp.org/EJS_template/App.js
+++ freecodecamp.org/EJS_template/App.js
@@ -7,8 +7,10 @@ const PORT = 3000
 app.use('/public', express.static(path.join(__dirname, '/views')))
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.get('/:userQuery', (req, res) => {
+    res.render('index', {data : {
+        userQuery : req.params.userQuery,
+    }})
 })
 
 app.listen(PORT, err => {
