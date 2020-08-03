faf2378e8768c8c5dd04baaedde09b269e90cba9
Garvit Saxena <garvitsaxena06@gmail.com>
2020-08-04 00:20:16 +0530

    calling custom middleware for a particular url

--- freecodecamp.org/Creating_own_middleware/App.js
+++ freecodecamp.org/Creating_own_middleware/App.js
@@ -3,6 +3,11 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+app.use('/hello', (req, res, next) => {
+    console.log(req.url, req.method)
+    next()
+})
+
 app.get('/', (req, res) => {
     res.send("Hello World!")
 })
